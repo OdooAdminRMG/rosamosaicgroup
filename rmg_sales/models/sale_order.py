e8218d4defaa418d8a89c6c7a5465b7790347d2c
Odoo Aktiv <odoo@aktivsoftware.com>
2022-05-04 17:33:08 +0530

    [IMP] USER_STORY_43: set delivery address requried false, and raise usererror if delivery address not set in sale order

--- rmg_sales/models/sale_order.py
+++ rmg_sales/models/sale_order.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import _, models
+from odoo import _, models, fields, api
 from odoo.tools import format_datetime
+from odoo.exceptions import UserError
+from odoo.tools import float_is_zero, html_keep_url, is_html_empty
 
 
 class SaleOrder(models.Model):
@@ -23,6 +25,8 @@ class SaleOrder(models.Model):
         }
 
     def action_confirm(self):
+        if not self.partner_shipping_id:
+            raise UserError(_("Delivery address is not set."))
         tz = self.env.context.get('tz') or self.env.user.tz or 'UTC'
         locale = self.env.context.get('lang') or self.env.user.lang or 'en_US'
         # Pass context from action_confirm button to open wizard for Delivery date confirmation
@@ -53,3 +57,48 @@ class SaleOrder(models.Model):
             return self.with_context(copy_context).open_message_wizard(message)
 
         return super(SaleOrder, self).action_confirm()
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        """
+        Update the following fields when the partner is changed:
+        - Pricelist
+        - Payment terms
+        - Invoice address
+        - Delivery address
+        - Sales Team
+        """
+        if not self.partner_id:
+            self.update({
+                'partner_invoice_id': False,
+                'partner_shipping_id': False,
+                'fiscal_position_id': False,
+            })
+            return
+
+        self = self.with_company(self.company_id)
+
+        addr = self.partner_id.address_get(['delivery', 'invoice'])
+        partner_user = self.partner_id.user_id or self.partner_id.commercial_partner_id.user_id
+        values = {
+            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
+            'payment_term_id': self.partner_id.property_payment_term_id and self.partner_id.property_payment_term_id.id or False,
+            'partner_invoice_id': addr['invoice'],
+        }
+        user_id = partner_user.id
+        if not self.env.context.get('not_self_saleperson'):
+            user_id = user_id or self.env.context.get('default_user_id', self.env.uid)
+        if user_id and self.user_id.id != user_id:
+            values['user_id'] = user_id
+
+        if self.env['ir.config_parameter'].sudo().get_param('account.use_invoice_terms'):
+            if self.terms_type == 'html' and self.env.company.invoice_terms_html:
+                baseurl = html_keep_url(self.get_base_url() + '/terms')
+                values['note'] = _('Terms & Conditions: %s', baseurl)
+            elif not is_html_empty(self.env.company.invoice_terms):
+                values['note'] = self.with_context(lang=self.partner_id.lang).env.company.invoice_terms
+        if not self.env.context.get('not_self_saleperson') or not self.team_id:
+            values['team_id'] = self.env['crm.team'].with_context(
+                default_team_id=self.partner_id.team_id.id
+            )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)], user_id=user_id)
+        self.update(values)
