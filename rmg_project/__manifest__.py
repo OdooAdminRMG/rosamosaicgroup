1c8a1ac9f15185e3ca8a7faa6316a7303b1b7ef3
Odoo Aktiv <odoo@aktivsoftware.com>
2022-05-07 10:48:57 +0530

    [Add] #420 | rmg_project: Added the code to show Installation and Templating Calendars in Project Menu

---
+++ rmg_project/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "RMG Project Enhancements",
+    "summary": """RMG Project Enhancements""",
+    "description": """RMG Project Enhancements""",
+    "author": "S4 Solutions, LLC",
+    "website": "https://www.sfour.io/",
+    "category": "business",
+    "version": "15.0.2.0.1",
+    "depends": ["sale_project", "project", "calendar"],
+    "data": [
+        "views/calendar_event_view.xml",
+        "views/sale_order_view.xml",
+        "views/menu_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "sequence": 100,
+}
