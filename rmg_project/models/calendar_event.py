d8f0704ff7d5e906049ba6b08079bea7eb2102f6
Odoo Aktiv <odoo@aktivsoftware.com>
2022-05-09 19:56:04 +0530

    [FIX] #420 | rmg_project: Fix the issue regarding install and template calendar

--- rmg_project/models/calendar_event.py
+++ rmg_project/models/calendar_event.py
@@ -11,12 +11,14 @@ class CalendarEvent(models.Model):
 
     @api.onchange('name')
     def _onchange_installing(self):
+        ''' Onchange method written to check name of meeting include template or install'''
         if self.name:
             name = self.name.lower()
             self.is_installing = True if 'install' in name else False
             self.is_templating = True if 'template' in name else False
 
     def check_templating_installing(self):
+        ''' Created Server Action to update old Record to check wheather they nclude template or install in it'''
         for rec in self:
             if rec.name:
                 name = rec.name.lower()
