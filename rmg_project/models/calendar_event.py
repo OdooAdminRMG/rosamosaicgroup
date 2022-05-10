50ad95e5d34a99fb129a77532b7ceac1aa481ae9
Odoo Aktiv <odoo@aktivsoftware.com>
2022-05-10 13:18:34 +0530

    [FIX] #20 | rmg_project: Fix the issue regarding install and template calendar

--- rmg_project/models/calendar_event.py
+++ rmg_project/models/calendar_event.py
@@ -9,18 +9,30 @@ class CalendarEvent(models.Model):
     is_templating = fields.Boolean()
     is_installing = fields.Boolean()
 
-    @api.onchange('name')
-    def _onchange_installing(self):
-        ''' Onchange method written to check name of meeting include template or install'''
-        if self.name:
-            name = self.name.lower()
-            self.is_installing = True if 'install' in name else False
-            self.is_templating = True if 'template' in name else False
+    @api.model_create_multi
+    def create(self, vals_list):
+        '''
+         Override Create method  to check name of meeting include template or install
+
+        '''
+        res = super().create(vals_list)
+        res.check_templating_installing()
+        return res
+
+    def write(self, vals):
+        '''
+         Override Write method  to check name of meeting include template or install
+        '''
+        res = super(CalendarEvent, self).write(vals)
+        if vals.get('name'):
+            self.check_templating_installing()
+        return res
 
     def check_templating_installing(self):
-        ''' Created Server Action to update old Record to check wheather they nclude template or install in it'''
+        '''
+        Created Server Action to update old Record to check weather they include template or install in it
+        '''
         for rec in self:
-            if rec.name:
-                name = rec.name.lower()
-                rec.is_installing = True if 'install' in name else False
-                rec.is_templating = True if 'template' in name else False
+            name = rec.name and rec.name.lower() or ''
+            rec.is_installing = True if 'install' in name else False
+            rec.is_templating = True if 'template' in name else False
--- rmg_project/models/sale_order.py
+++ rmg_project/models/sale_order.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo, S4 Solutions, LLC.
+# See LICENSE file for full copyright & licensing details.
 
-from odoo import fields, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
+    """
+
+    """
     _inherit = "sale.order"
 
     def action_calendar_installation_view(self):
+        """
+
+        :return:
+        """
+        context = self._context.copy()
+        context.update({"default_model_id": "sale.order"})
         action_data = self.env.ref(
-            "rmg_project.action_calendar_event_project_installation").read()[0]
-        action_data.update(
-            {
-
-                "context": {
-                    "default_model_id": "sale.order",
-
-                },
-            }
-        )
+            "rmg_project.action_calendar_event_project_installation"
+        ).read()[0]
+        action_data['context'] = context
         return action_data
