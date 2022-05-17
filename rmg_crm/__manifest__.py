67db30482c83bed0d395ae443a6ce77519169dc3
odoo <khyati@aktivsoftware.com>
2022-05-17 18:36:06 +0530

    [MOD]rmg_crm rmg_us_15

--- rmg_crm/__manifest__.py
+++ rmg_crm/__manifest__.py
@@ -7,16 +7,20 @@
     "author": "S4 Solutions, LLC",
     "website": "https://www.sfour.io/",
     "category": "sales",
-    "version": "15.0.2.2.1",
+    "version": "15.0.2.3.1",
     "depends": ["sale_management", "sale_crm", "project", "stock", "purchase", "mrp"],
     "data": [
+        "security/ir.model.access.csv",
+        "views/ir_attachment_type_views.xml",
+        "views/ir_attachment_views.xml",
+        "views/crm_views.xml",
         "views/rmg_sale_views.xml",
         "views/rmg_mrp_views.xml",
         "views/rmg_project_views.xml",
         "views/rmg_stock_picking_views.xml",
     ],
-    'assets': {
-        'web.assets_qweb': [
+    "assets": {
+        "web.assets_qweb": [
             # 'rmg_crm/static/src/xml/web_calendar.xml',
         ],
     },
