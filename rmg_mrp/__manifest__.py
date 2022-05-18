25b499f52c98c7523f12af68067c19e76c17b1d0
odoo <khyati@aktivsoftware.com>
2022-05-18 16:00:16 +0530

    [MOD]rmg_mrp modify view

--- rmg_mrp/__manifest__.py
+++ rmg_mrp/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "sales",
     "summary": "",
     "sequence": 20,
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     'author': "S4 Solutions, LLC",
     'website': "https://www.sfour.io/",
     "depends": ["stock", "sale_management", "mrp"],
