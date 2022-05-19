58ea628298bb713ad470b495bd116cb30f23d95a
odoo <khyati@aktivsoftware.com>
2022-05-19 14:08:34 +0530

    [MOD]rmg_project modify project kanban view

---
+++ rmg_project/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, S4 Solutions, LLC.
+# See LICENSE file for full copyright & licensing details.
+
+{
+    "name": "RMG Project Enhancements",
+    "summary": """RMG Project Enhancements""",
+    "description": """RMG Project Enhancements""",
+    "author": "S4 Solutions, LLC",
+    "website": "https://www.sfour.io/",
+    "category": "business",
+    "version": "15.0.0.0.0",
+    "depends": [
+        "sale_project",
+    ],
+    "data": [
+        "views/project_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "sequence": 100,
+}
