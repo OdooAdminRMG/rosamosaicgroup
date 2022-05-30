b51345ba6ff5aea7b2075015d119dd4deb1ee87e
Odoo Akiv <odoo@aktivsoftware.com>
2022-05-30 10:38:33 +0530

    [IMP] improved resequence with gettable context from request if exists

--- rmg_project/controllers/main.py
+++ rmg_project/controllers/main.py
@@ -1,12 +1,12 @@
-from odoo import http
-
 from odoo.addons.web.controllers.main import DataSet
+from odoo import http
+from odoo.http import request
 
 
 class ResequenceDataset(DataSet):
 
 	@http.route('/web/dataset/resequence', type='json', auth="user")
 	def resequence(self, model, ids, field='sequence', offset=0):
-		if model != 'project.project' and not self._context.get('search_default_groupby_stage', False):
-			return super(ResequenceDataset, self).resequence()
+		if model != 'project.project' and hasattr(request, '_context') and not request._context.get('search_default_groupby_stage', False):
+			return super(ResequenceDataset, self).resequence(model, ids, field, offset)
 		return True
