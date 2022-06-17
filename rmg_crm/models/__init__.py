baa4a03bc71afe32f00276c3f166ff30eaac4255
odoo <khyati@aktivsoftware.com>
2022-06-17 16:11:30 +0530

    [MOD]rmg_crm code commenting and add help.

--- rmg_crm/models/__init__.py
+++ rmg_crm/models/__init__.py
@@ -1,2 +1,19 @@
-from . import (crm_lead, ir_attachment, ir_attachment_type, mrp_production,
-               project_project, replenish_sources, purchase_order, sale_order, stock_picking, stock_rule, job_costing)
+"""
+    File             -->     Depends on
+    attachment_type  -->     ir_attachment
+    job_costing      -->     sale_order,replenish_sources
+"""
+
+from . import (
+    crm_lead,
+    ir_attachment,
+    ir_attachment_type,
+    mrp_production,
+    project_project,
+    replenish_sources,
+    purchase_order,
+    sale_order,
+    stock_picking,
+    stock_rule,
+    job_costing,
+)
