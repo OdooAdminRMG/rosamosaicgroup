4b220c6b9acf49f3a0a8e9a2744ef4f7d5adef3e
odoo <khyati@aktivsoftware.com>
2022-06-21 10:25:21 +0530

    [MOD]rmg_crm change file name

--- rmg_crm/models/attachment_type.py
+++ rmg_crm/models/attachment_type.py
@@ -2,8 +2,8 @@ from odoo import _, fields, models
 
 
 class IrAttachmentType(models.Model):
-    _name = "attachment.type"
-    _description = "Ir Attachment Type"
+    _name = "ir.attachment.type"
+    _description = "Attachment Type"
     _rec_name = "name"
 
     name = fields.Char(string=_("Name"), required=True)
