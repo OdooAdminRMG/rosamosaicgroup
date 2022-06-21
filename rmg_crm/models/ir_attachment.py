f0614ae1c55836e5c37e895a1e566e24009e67c7
odoo <khyati@aktivsoftware.com>
2022-06-21 10:35:51 +0530

    [MOD]rmg_crm bug fic

--- rmg_crm/models/ir_attachment.py
+++ rmg_crm/models/ir_attachment.py
@@ -4,5 +4,4 @@ from odoo import _, fields, models
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
-    # Provide inverse relation to attachment type.
-    type_id = fields.Many2one("attachment.type", string=_("Attachment Type"))
+    type_id = fields.Many2one("ir.attachment.type", string=_("Attachment Type"))
