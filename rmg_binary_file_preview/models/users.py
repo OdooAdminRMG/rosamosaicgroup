76ee970938c82840307a69169aa4fbb06d9e5bb2
Odoo Aktiv <odoo@aktivsoftware.com>
2022-07-04 14:17:26 +0530

    [UPDATE]:Attachment download from crm and MRP

--- rmg_binary_file_preview/models/users.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class Users(models.Model):
-    _inherit = 'res.users'
-
-    allow_preview = fields.Boolean(string='Allow Preview',
-                                   help='It will allow the user to access all attachments')
