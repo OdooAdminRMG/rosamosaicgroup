412e896883e313ee7dea31930c233d94e234ba1e
odoo <khyati@aktivsoftware.com>
2022-07-04 17:22:01 +0530

    [MOD]rmg_crm

--- rmg_crm/models/__init__.py
+++ rmg_crm/models/__init__.py
@@ -1,2 +1,3 @@
 from . import (crm_lead, ir_attachment, ir_attachment_type, mrp_production,
                project_project, replenish_sources, purchase_order, sale_order, stock_picking, stock_rule, job_costing, account_move)
+
