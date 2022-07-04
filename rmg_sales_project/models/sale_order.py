e3a3f3e713d33a8cb5d8aa85dd3a44c7131aa42c
odoo <khyati@aktivsoftware.com>
2022-07-04 18:34:51 +0530

    [MOD]rmg_sales_project convert into string

--- rmg_sales_project/models/sale_order.py
+++ rmg_sales_project/models/sale_order.py
@@ -65,161 +65,161 @@ class SaleOrder(models.Model):
                 orders
             )
         if self.commitment_date:
-            so_commitment_date = datetime.strptime(self.commitment_date, DTS)
+            so_commitment_date = datetime.strptime(str(self.commitment_date), DTS)
             # Clear all dates on Project task
             for project in self.project_ids:
                 project.tasks.planned_date_begin = False
                 project.tasks.planned_date_end = False
             self.calculate_planned_dates(so_commitment_date)
 
-def get_attendances(self, start_date):
-    resource_id = self.env.user.resource_ids[0] if self.env.user.resource_ids else self.env['resource.resource']
-    attendances = resource_id.calendar_id.attendance_ids.filtered(
-        lambda a: a.dayofweek == str(start_date.weekday()))
-    return resource_id, attendances, resource_id.calendar_id.attendance_ids
+    def get_attendances(self, start_date):
+        resource_id = self.env.user.resource_ids[0] if self.env.user.resource_ids else self.env['resource.resource']
+        attendances = resource_id.calendar_id.attendance_ids.filtered(
+            lambda a: a.dayofweek == str(start_date.weekday()))
+        return resource_id, attendances, resource_id.calendar_id.attendance_ids
 
 
-def get_start_date(self, start_date, hours):
-    resource_id, attendances, all_attendance_ids = self.get_attendances(start_date)
-    hours_per_day = resource_id.calendar_id._compute_hours_per_day(attendances)
-    if hours >= hours_per_day:
-        hours -= hours_per_day
-        return self.get_start_date(get_next_or_last_working_days_count(start_date, all_attendance_ids), hours)
-    start_date = self.adjust_dates_in_user_working_time(start_date - relativedelta(hours=hours), hours=hours)
-    return start_date.replace(tzinfo=None)
-
-
-def get_working_start_end_date(self, start_date):
-    """
-
-    :param start_date: task start date
-    :param end_date: task end date
-    :return: working hour start date, end date, all week records
-    """
-    resource_id = self.env.user.resource_ids[0] if self.env.user.resource_ids else self.env['resource.resource']
-    working_start_date = datetime.combine(start_date.date(), time.min).replace(tzinfo=UTC)
-    working_end_date = datetime.combine(start_date.date(), time.max).replace(tzinfo=UTC)
-    work_intervals_batch = resource_id.calendar_id._work_intervals_batch(working_start_date, working_end_date,
-                                                                         resources=resource_id)
-    work_intervals = [(start, stop) for start, stop, dummy in work_intervals_batch.get(resource_id.id, False)]
-    if work_intervals:
-        working_start_date = work_intervals[0][0].astimezone(UTC)
-        working_end_date = work_intervals[-1][-1].astimezone(UTC)
-    return working_start_date, working_end_date
-
-
-def adjust_dates_in_user_working_time(self, start_date, hours=0):
-    """
-        This method will get the resource calendar and calculate working time and adjust dates accordingly
-        :param start_date: Task start date
-        :param end_date: Task end date
-        :return: adjusted start and end dates
-    """
-    start_date = start_date.replace(tzinfo=UTC)
-    working_start_date, working_end_date = self.get_working_start_end_date(
-        start_date.astimezone(timezone(self.env.user.tz)).replace(tzinfo=None))
-    # Custom logic to adjust start date and end date in working time
-    if start_date < working_start_date:
+    def get_start_date(self, start_date, hours):
         resource_id, attendances, all_attendance_ids = self.get_attendances(start_date)
-        if start_date.date() == working_start_date.date():
-            last_day_start_date = get_next_or_last_working_days_count(start_date, all_attendance_ids)
-        else:
-            if start_date.weekday() not in list(map(int, all_attendance_ids.mapped('dayofweek'))):
+        hours_per_day = resource_id.calendar_id._compute_hours_per_day(attendances)
+        if hours >= hours_per_day:
+            hours -= hours_per_day
+            return self.get_start_date(get_next_or_last_working_days_count(start_date, all_attendance_ids), hours)
+        start_date = self.adjust_dates_in_user_working_time(start_date - relativedelta(hours=hours), hours=hours)
+        return start_date.replace(tzinfo=None)
+
+
+    def get_working_start_end_date(self, start_date):
+        """
+
+        :param start_date: task start date
+        :param end_date: task end date
+        :return: working hour start date, end date, all week records
+        """
+        resource_id = self.env.user.resource_ids[0] if self.env.user.resource_ids else self.env['resource.resource']
+        working_start_date = datetime.combine(start_date.date(), time.min).replace(tzinfo=UTC)
+        working_end_date = datetime.combine(start_date.date(), time.max).replace(tzinfo=UTC)
+        work_intervals_batch = resource_id.calendar_id._work_intervals_batch(working_start_date, working_end_date,
+                                                                             resources=resource_id)
+        work_intervals = [(start, stop) for start, stop, dummy in work_intervals_batch.get(resource_id.id, False)]
+        if work_intervals:
+            working_start_date = work_intervals[0][0].astimezone(UTC)
+            working_end_date = work_intervals[-1][-1].astimezone(UTC)
+        return working_start_date, working_end_date
+
+
+    def adjust_dates_in_user_working_time(self, start_date, hours=0):
+        """
+            This method will get the resource calendar and calculate working time and adjust dates accordingly
+            :param start_date: Task start date
+            :param end_date: Task end date
+            :return: adjusted start and end dates
+        """
+        start_date = start_date.replace(tzinfo=UTC)
+        working_start_date, working_end_date = self.get_working_start_end_date(
+            start_date.astimezone(timezone(self.env.user.tz)).replace(tzinfo=None))
+        # Custom logic to adjust start date and end date in working time
+        if start_date < working_start_date:
+            resource_id, attendances, all_attendance_ids = self.get_attendances(start_date)
+            if start_date.date() == working_start_date.date():
                 last_day_start_date = get_next_or_last_working_days_count(start_date, all_attendance_ids)
             else:
-                last_day_start_date = start_date
-        hours = (working_start_date - start_date).seconds / 3600
-        working_start_date, working_end_date = self.get_working_start_end_date(last_day_start_date)
-        start_date = working_end_date - relativedelta(hours=hours)
+                if start_date.weekday() not in list(map(int, all_attendance_ids.mapped('dayofweek'))):
+                    last_day_start_date = get_next_or_last_working_days_count(start_date, all_attendance_ids)
+                else:
+                    last_day_start_date = start_date
+            hours = (working_start_date - start_date).seconds / 3600
+            working_start_date, working_end_date = self.get_working_start_end_date(last_day_start_date)
+            start_date = working_end_date - relativedelta(hours=hours)
 
-    return start_date.replace(tzinfo=None)
+        return start_date.replace(tzinfo=None)
 
 
-def calculate_planned_dates(self, commitment_date):
-    """
-    Update task's start and end date
-    :param commitment_date: Date from quotation deadline date
-    :return: None
-    """
-    # Adjust the commitment date as per the working hours
-    commitment_date = self.adjust_dates_in_user_working_time(commitment_date)
-
-    def get_depend_on_task_list(final_task, task_depend_on_dict, commitment_date, first_task):
+    def calculate_planned_dates(self, commitment_date):
         """
-        Recursion method to update all child tasks
-        :param final_task: Task in which start and end date should be placed
-        :param task_depend_on_dict: dependent tasks' dictionary
-        :param commitment_date: Deadline date
-        :param previous_task: previous task to get offset hours
-        :return: updated task
+        Update task's start and end date
+        :param commitment_date: Date from quotation deadline date
+        :return: None
         """
-        index = [final_task]
-        if final_task and final_task in task_depend_on_dict:
-            # Check again start and end date because it may be possible that after subtracting start date may go beyond working time so setting as per the working hours
-            date_end = self.get_start_date(commitment_date, final_task.offset_hours)
-            date_begin = self.get_start_date(date_end if not first_task else commitment_date, final_task.lead_time)
-            if not final_task.planned_date_begin or final_task.planned_date_begin > commitment_date:
-                final_task.planned_date_begin = date_begin
-                final_task.planned_date_end = commitment_date if first_task else date_end
-            for inner_elem in task_depend_on_dict.get(final_task):
-                index.extend(get_depend_on_task_list(inner_elem, task_depend_on_dict, date_begin, first_task=False))
-        return index
+        # Adjust the commitment date as per the working hours
+        commitment_date = self.adjust_dates_in_user_working_time(commitment_date)
 
-    if commitment_date:
-        for project in self.project_ids:
-            all_task_list = []
-            # get all depended_task_ids
-            depended_task_ids = [depend_task for task in project.tasks for depend_task in task.depend_on_ids]
-            task_depend_on_dict = {task: task.depend_on_ids for task in project.tasks}
-            # Manage final task by which doesn't set as depended_task
-            final_task_ids = set(project.tasks) - set(depended_task_ids)
-            for index, final_task in enumerate(sorted(final_task_ids, key=lambda x: x.sequence)):
-                final_task_depends_list = get_depend_on_task_list(final_task, task_depend_on_dict, commitment_date,
-                                                                  first_task=True)
-                all_task_list.extend(final_task_depends_list)
+        def get_depend_on_task_list(final_task, task_depend_on_dict, commitment_date, first_task):
+            """
+            Recursion method to update all child tasks
+            :param final_task: Task in which start and end date should be placed
+            :param task_depend_on_dict: dependent tasks' dictionary
+            :param commitment_date: Deadline date
+            :param previous_task: previous task to get offset hours
+            :return: updated task
+            """
+            index = [final_task]
+            if final_task and final_task in task_depend_on_dict:
+                # Check again start and end date because it may be possible that after subtracting start date may go beyond working time so setting as per the working hours
+                date_end = self.get_start_date(commitment_date, final_task.offset_hours)
+                date_begin = self.get_start_date(date_end if not first_task else commitment_date, final_task.lead_time)
+                if not final_task.planned_date_begin or final_task.planned_date_begin > commitment_date:
+                    final_task.planned_date_begin = date_begin
+                    final_task.planned_date_end = commitment_date if first_task else date_end
+                for inner_elem in task_depend_on_dict.get(final_task):
+                    index.extend(get_depend_on_task_list(inner_elem, task_depend_on_dict, date_begin, first_task=False))
+            return index
 
-
-def write(self, vals):
-    """
-    Override to update commitment date If changed and re-calculate
-    :param vals: Values to write in task
-    :return: Boolean
-    """
-    res = super(SaleOrder, self).write(vals)
-    if 'commitment_date' in vals and vals['commitment_date']:
-        so_commitment_date = datetime.strptime(vals['commitment_date'], DTS)
-        # Clear all dates on Project task
-        for project in self.project_ids:
-            project.tasks.planned_date_begin = False
-            project.tasks.planned_date_end = False
-        self.calculate_planned_dates(so_commitment_date)
-    return res
+        if commitment_date:
+            for project in self.project_ids:
+                all_task_list = []
+                # get all depended_task_ids
+                depended_task_ids = [depend_task for task in project.tasks for depend_task in task.depend_on_ids]
+                task_depend_on_dict = {task: task.depend_on_ids for task in project.tasks}
+                # Manage final task by which doesn't set as depended_task
+                final_task_ids = set(project.tasks) - set(depended_task_ids)
+                for index, final_task in enumerate(sorted(final_task_ids, key=lambda x: x.sequence)):
+                    final_task_depends_list = get_depend_on_task_list(final_task, task_depend_on_dict, commitment_date,
+                                                                      first_task=True)
+                    all_task_list.extend(final_task_depends_list)
 
 
-def action_confirm(self):
-    """
-    Re-calculate planned dates on confirm of sales order
-    :return:
-    """
-    res = super(SaleOrder, self).action_confirm()
-    if self.commitment_date:
-        self.calculate_planned_dates(commitment_date=self.commitment_date)
-    project_task_mo = self.tasks_ids.filtered(
-        lambda p: p.peg_to_manufacturing_order
-    )
-    project_task_do = self.tasks_ids.filtered(
-        lambda p: p.peg_to_delivery_order
-    )
-    move_ids = self.env['procurement.group'].search([
-        ('sale_id', 'in', self.ids)
-    ]).stock_move_ids
-    move_ids.created_production_id.project_task_id = project_task_mo.id
-    picking_id = move_ids.picking_id.filtered(lambda x: x.picking_type_id.code == 'outgoing')
-    picking_id.project_task_id = project_task_do.id
-    if project_task_do.planned_date_end and picking_id:
-        picking_id.scheduled_date = project_task_do.planned_date_end
-        picking_id.date_deadline = project_task_do.planned_date_end
-    if move_ids.created_production_id and project_task_mo.planned_date_begin and project_task_mo.planned_date_end:
-        move_ids.created_production_id.date_planned_start = project_task_mo.planned_date_begin
-        move_ids.created_production_id.date_deadline = project_task_mo.planned_date_end
-    return res
+    def write(self, vals):
+        """
+        Override to update commitment date If changed and re-calculate
+        :param vals: Values to write in task
+        :return: Boolean
+        """
+        res = super(SaleOrder, self).write(vals)
+        if 'commitment_date' in vals and vals['commitment_date']:
+            so_commitment_date = datetime.strptime(vals['commitment_date'], DTS)
+            # Clear all dates on Project task
+            for project in self.project_ids:
+                project.tasks.planned_date_begin = False
+                project.tasks.planned_date_end = False
+            self.calculate_planned_dates(so_commitment_date)
+        return res
+
+
+    def action_confirm(self):
+        """
+        Re-calculate planned dates on confirm of sales order
+        :return:
+        """
+        res = super(SaleOrder, self).action_confirm()
+        if self.commitment_date:
+            self.calculate_planned_dates(commitment_date=self.commitment_date)
+        project_task_mo = self.tasks_ids.filtered(
+            lambda p: p.peg_to_manufacturing_order
+        )
+        project_task_do = self.tasks_ids.filtered(
+            lambda p: p.peg_to_delivery_order
+        )
+        move_ids = self.env['procurement.group'].search([
+            ('sale_id', 'in', self.ids)
+        ]).stock_move_ids
+        move_ids.created_production_id.project_task_id = project_task_mo.id
+        picking_id = move_ids.picking_id.filtered(lambda x: x.picking_type_id.code == 'outgoing')
+        picking_id.project_task_id = project_task_do.id
+        if project_task_do.planned_date_end and picking_id:
+            picking_id.scheduled_date = project_task_do.planned_date_end
+            picking_id.date_deadline = project_task_do.planned_date_end
+        if move_ids.created_production_id and project_task_mo.planned_date_begin and project_task_mo.planned_date_end:
+            move_ids.created_production_id.date_planned_start = project_task_mo.planned_date_begin
+            move_ids.created_production_id.date_deadline = project_task_mo.planned_date_end
+        return res
