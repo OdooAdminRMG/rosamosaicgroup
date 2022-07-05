d0f93ad82d26c0fc11d75cce7044661c8c3dd949
odoo <khyati@aktivsoftware.com>
2022-07-05 15:32:35 +0530

    [MOD]rmg_sales_project development done

--- rmg_sales_project/models/sale_order.py
+++ rmg_sales_project/models/sale_order.py
@@ -99,9 +99,6 @@ class SaleOrder(models.Model):
                 move_ids.created_production_id.date_planned_start = project_task_mo.planned_date_begin
                 move_ids.created_production_id.date_deadline = project_task_mo.planned_date_end
 
-    template_start_date = fields.Date(string='Template Start Date')
-    template_end_date = fields.Date(string='Template Start Date', index=True, tracking=True)
-
     def get_attendances(self, start_date):
         resource_id = self.env.user.resource_ids[0] if self.env.user.resource_ids else self.env['resource.resource']
         attendances = resource_id.calendar_id.attendance_ids.filtered(
