9f50eec5be0f034267ef3a917cadaa8a1d7a89af
odoo <khyati@aktivsoftware.com>
2022-07-15 18:21:43 +0530

    [MOD]rmg_crm Code refactore.

--- rmg_crm/models/job_costing.py
+++ rmg_crm/models/job_costing.py
@@ -32,12 +32,12 @@ class JobCostingReport(models.Model):
     contract_amount = fields.Float(
         string=_("Contract Amount"),
         help="Compute the sum of amount_total of all Sale Orders  based on 'Job Name' "
-             "and 'End Date'(if end date is entered in filter)."
+             "and 'End Date'(if end date filter is selected)."
     )
     billings_to_date = fields.Float(
         string=_("Billings To Date"),
         help="Compute the sum of 'Amount Total' of all Invoices whose related Sale Orders has the same 'Job Name'."
-             "and 'End Date'(if end date is entered in filter)."
+             "and 'End Date'(if end date filter is selected)."
     )
     remaining = fields.Float(
         string=_("Remaining"),
@@ -46,23 +46,26 @@ class JobCostingReport(models.Model):
     total_cost_of_purchase = fields.Float(
         string=_("Total Cost Of Purchase"),
         help="Compute the sum of (Product Uom Qty * Unit Price) of all Replenish Histories based on 'Job Name'."
+             "and 'End Date'(if end date filter is selected)."
     )
     total_cost_of_components = fields.Float(
         string=_("Total Cost Of Components"),
-        help="Compute the sum of 'Total Cost' of all Manufacturing Orders based on job_name."
+        help="Compute the sum of 'Total Cost' of all Manufacturing Orders based on Job name."
+             "and 'End Date'(if end date filter is selected)."
     )
     total_cost_of_purchase_timesheet = fields.Float(
         string=_("Total Cost Of Timesheet"),
         help="Compute the sum of (Unit Amount * Timesheet Cost of that employee) of all Tasks  based on 'Job Name'."
+             "and 'End Date'(if end date filter is selected)."
     )
     actual = fields.Float(
         string=_("Actual"),
         help="Compute the sum of 'Total Cost Of Purchase', 'Total Cost Of Components' and 'Total Cost Of Timesheet'"
     )
-
     budget = fields.Float(
         string=_("Budget"),
         help="Calculate the sum of 'Budgeted Labor Cost' and 'Budgeted Material Cost' based on 'Job Name'."
+             "and 'End Date'(if end date filter is selected)."
     )
     pct = fields.Float(
         string=_("Pct"),
@@ -74,37 +77,61 @@ class JobCostingReport(models.Model):
     )
 
     def calculate_values_from_sale_order(self):
-
+        """
+            This method will calculate values of fields which depends on sale order.
+        """
+        so = self.sale_id
         filter_date = self.filter_date
-        sale_orders = self.env['sale.order'].search(
-            [
-                ('job_name', '=', self.job_name)
-            ]
-        )
 
         # Compute the sum of amount_total of all Sale Orders  based on 'Job Name' and 'End Date'.
-        self.contract_amount = sum(
-            sale_orders.mapped(
-                lambda so: so.amount_total
-                if (
-                        (
-                                filter_date
-                                and so.create_date <= filter_date
-                        )
-                        or not filter_date
+        self.contract_amount, self.budget = (so.amount_total, so.budgeted_labor_cost + so.budgeted_material_cost) if (
+                (
+                        filter_date
+                        and so.create_date <= filter_date
                 )
-                else 0
-            )
-        )
+                or not filter_date
+        ) else (0, 0)
 
-        self.budget = sum(
-            sale_orders.mapped(
-                lambda so: so.budgeted_labor_cost + so.budgeted_material_cost
-            )
-        )
+    # code backup
+    # If we get multiple sale orders with same job name then we can use below code.
+
+    # def calculate_values_from_sale_order(self):
+    #     """
+    #         This method will calculate values of fields which depends on sale order
+    #     """
+    #     filter_date = self.filter_date
+    #     sale_orders = self.env['sale.order'].search(
+    #         [
+    #             ('job_name', '=', self.job_name)
+    #         ]
+    #     )
+    #
+    #     # Compute the sum of amount_total of all Sale Orders  based on 'Job Name' and 'End Date'.
+    #     self.contract_amount = sum(
+    #         sale_orders.mapped(
+    #             lambda so: so.amount_total
+    #             if (
+    #                     (
+    #                             filter_date
+    #                             and so.create_date <= filter_date
+    #                     )
+    #                     or not filter_date
+    #             )
+    #             else 0
+    #         )
+    #     )
+    #
+    #     self.budget = sum(
+    #         sale_orders.mapped(
+    #             lambda so: so.budgeted_labor_cost + so.budgeted_material_cost
+    #         )
+    #     )
 
     def compute_billings_to_date(self):
-        self.billings_to_date = sum(
+        """
+            Return the sum of amount_total of all invoices related to sale order.
+        """
+        return sum(
             self.env['account.move'].search(
                 [
                     ('job_name', '=', self.job_name)
@@ -123,10 +150,12 @@ class JobCostingReport(models.Model):
             )
         )
 
-    def compute_remaining(self):
-        self.remaining = self.contract_amount - self.billings_to_date
-
     def compute_actual(self):
+        """
+            This method will calculate and return sum of
+            'Total Cost Of Purchase', 'Total Cost Of Components' and 'Total Cost Of Timesheet'
+            based on 'job_name' and 'filter_date'.
+        """
         filter_date = self.filter_date
         job_name = self.job_name
         total_cost_of_purchase = 0
@@ -197,67 +226,81 @@ class JobCostingReport(models.Model):
         self.total_cost_of_purchase = total_cost_of_purchase
         self.total_cost_of_components = cal_actual[0]
         self.total_cost_of_purchase_timesheet = cal_actual[1]
-        self.actual = sum(cal_actual) + total_cost_of_purchase
-
-    def compute_pct(self):
-        budget = self.budget
-        self.pct = float(self.actual / budget if budget else 0.0)
-
-    def compute_overrun(self):
-        self.overrun = self.actual - self.budget
+        return sum(cal_actual) + total_cost_of_purchase
 
     @api.depends('filter_date', 'job_name')
     def _compute_all_fields(self):
+        """
+            This method will calculate all fields based on 'job_name' and 'filter_date'.
+        """
         for rec in self:
             rec.calculate_values_from_sale_order()
-            rec.compute_billings_to_date()
-            rec.compute_remaining()
-            rec.compute_actual()
-            rec.compute_pct()
-            rec.compute_overrun()
+            billings_to_date = rec.compute_billings_to_date()
+            rec.remaining = rec.contract_amount - billings_to_date
+            actual = rec.compute_actual()
+            budget = rec.budget
+            rec.pct = float(actual / budget if budget else 0.0)
+            rec.overrun = actual - budget
+            rec.billings_to_date = billings_to_date
+            rec.actual = actual
             rec.compute_all_fields = ''
 
-    def get_filter_values_from_domain(self, domain):
-        end_date_filter_string = [
-            filter_val[2]
-            for filter_val in domain
-            if len(filter_val) == 3 and filter_val[0] == 'create_date'
-        ]
-        if end_date_filter_string:
+    def calculate_filter_date_from_domain(self, domain):
+        """
+            If domain contains filter on 'job_name' and a string starts with 'RMG_custom_filter *'
+            Then it will calculate and return 'filter_date'
+        """
+        display_line_filter_string = [filter_val[2] for filter_val in domain if
+                                      len(filter_val) == 3 and filter_val[0] == 'job_name']
+        if 'RMG_custom_filter End of Last Financial Year' in display_line_filter_string:
+            return datetime.date.today().replace(month=1, day=1) - datetime.timedelta(days=1)
+        elif 'RMG_custom_filter End of Last Month' in display_line_filter_string:
+            return datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+        elif 'RMG_custom_filter End of Last Quarter' in display_line_filter_string:
+            today = datetime.date.today()
+            if today.month < 4:
+                return datetime.date(today.year - 1, 12, 31)
+            elif today.month < 7:
+                return datetime.date(today.year, 3, 31)
+            elif today.month < 10:
+                return datetime.date(today.year, 6, 30)
+            else:
+                return datetime.date(today.year, 9, 30)
+        elif 'RMG_custom_filter Today' in display_line_filter_string:
+            return datetime.datetime.now()
+        else:
+            return False
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None, **read_kwargs):
+        """
+            This method will create or remove records on job_costing_report.
+            This method will update 'filter_date' if user selects any custom filters of 'End Date'.
+        """
+        order = self._order
+        if domain:
+            custom_filter = self.calculate_filter_date_from_domain(domain)
             self.env.cr.execute(
                 """UPDATE %s SET
                 filter_date = '%s'""" % (
                     self._table,
-                    end_date_filter_string[0],
+                    custom_filter
+                )
+            ) if custom_filter else self.env.cr.execute(
+                """UPDATE %s SET
+                filter_date = NULL""" % (
+                    self._table
                 )
             )
-            return False
         else:
-            display_line_filter_string = [filter_val[2] for filter_val in domain if
-                                          len(filter_val) == 3 and filter_val[0] == 'job_name']
-            if 'RMG_custom_filter End of Last Financial Year' in display_line_filter_string:
-                return datetime.date.today().replace(month=1, day=1) - datetime.timedelta(days=1)
-            elif 'RMG_custom_filter End of Last Month' in display_line_filter_string:
-                return datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
-            elif 'RMG_custom_filter End of Last Quarter' in display_line_filter_string:
-                today = datetime.date.today()
-                if today.month < 4:
-                    return datetime.date(today.year - 1, 12, 31)
-                elif today.month < 7:
-                    return datetime.date(today.year, 3, 31)
-                elif today.month < 10:
-                    return datetime.date(today.year, 6, 30)
-                else:
-                    return datetime.date(today.year, 9, 30)
-            elif 'RMG_custom_filter Today' in display_line_filter_string:
-                return datetime.datetime.now()
-            else:
-                return False
-
-    @api.model
-    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None, **read_kwargs):
+            self.env.cr.execute(
+                """UPDATE %s SET
+                filter_date = NULL""" % (
+                    self._table,
+                )
+            )
+        rtn = super(JobCostingReport, self).search_read(domain, fields, offset, limit, order, **read_kwargs)
         if self._name == "job.costing.report":
-            order = self._order
             self._cr.execute(
                 """
                 INSERT INTO %s (sale_id, job_name)
@@ -277,31 +320,7 @@ class JobCostingReport(models.Model):
                     self._table
                 )
             )
-            if domain:
-                custom_filter = self.get_filter_values_from_domain(domain)
-                if custom_filter:
-                    self.env.cr.execute(
-                        """UPDATE %s SET
-                        filter_date = '%s'""" % (
-                            self._table,
-                            custom_filter
-                        )
-                    )
-                else:
-                    self.env.cr.execute(
-                        """UPDATE %s SET
-                        filter_date = NULL""" % (
-                            self._table
-                        )
-                    )
-            else:
-                self.env.cr.execute(
-                    """UPDATE %s SET
-                    filter_date = NULL""" % (
-                        self._table,
-                    )
-                )
-        return super(JobCostingReport, self).search_read(domain, fields, offset, limit, order, **read_kwargs)
+        return rtn
 
     @api.model
     def fields_get(self, fields=None):
