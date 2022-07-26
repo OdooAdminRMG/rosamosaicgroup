9a22d1ad163d21ecb156287809395a51d5a979a5
odoo <khyati@aktivsoftware.com>
2022-07-26 12:29:31 +0530

    [MOD]rmg_crm update filter and improve execution speed

--- rmg_crm/models/__init__.py
+++ rmg_crm/models/__init__.py
@@ -1,9 +1,22 @@
 """
-    File             -->     Depends on
-    attachment_type  -->     ir_attachment
-    job_costing      -->     sale_order,replenish_sources
+    File Name           -->     Depends on
+    attachment_type     -->     ir_attachment
+    job_costing_filter  -->     job_costing
+    job_costing         -->     sale_order,replenish_sources
 """
 
-from . import (account_move, attachment_type, crm_lead, ir_attachment,
-               job_costing, mrp_production, project_project, purchase_order,
-               replenish_sources, sale_order, stock_picking, stock_rule)
+from . import (
+    account_move,
+    attachment_type,
+    crm_lead,
+    ir_attachment,
+    job_costing,
+    job_costing_filter,
+    mrp_production,
+    project_project,
+    purchase_order,
+    replenish_sources,
+    sale_order,
+    stock_picking,
+    stock_rule,
+)
