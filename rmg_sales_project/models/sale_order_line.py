095baf06cd9145e6635775d95c784164350b0c9b
KhyatiAktiv <khyati@aktivsoftware.com>
2022-09-05 11:01:10 +0530

    [MOD]create_project_from_so add comments

--- rmg_sales_project/models/sale_order_line.py
+++ rmg_sales_project/models/sale_order_line.py
@@ -8,6 +8,8 @@ class SaleOrderLine(models.Model):
 
     def unlink(self):
         if self.project_id:
+            # Stander odoo don't delete or archive project associated with order line.
+            # So, in order to prevent single turn error we need to raise user error to prevent.
             raise UserError(_("You cannot delete a order line containing project. "
                               "You can either archive it or first delete its project."))
         return super(SaleOrderLine, self).unlink()
