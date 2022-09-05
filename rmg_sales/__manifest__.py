3716a6234b780f3471e8171dd36a4f78a4b00570
odooaktiv <odoo@aktivsoftware.com>
2022-09-05 10:09:20 +0530

    [FIX]: Solve merge conflict in Manifest file.

--- rmg_sales/__manifest__.py
+++ rmg_sales/__manifest__.py
@@ -6,11 +6,8 @@
     "description": """
             Manage Delivery Dates on Sale Order
             
-<<<<<<< HEAD
-            User Stories: 21, 24, 61
-=======
+
             User Stories: 21, 24, 61, 58
->>>>>>> 2727db268a2f47b31d7d6bb4f7f0ee5cadce0702
             """,
     "author": "S4 Solutions, LLC",
     "website": "https://www.sfour.io/",
@@ -20,13 +17,9 @@
     "data": [
         "security/ir.model.access.csv",
         "reports/rmg_delivery_slip.xml",
-<<<<<<< HEAD
         "reports/rmg_sale_order_report.xml",
-        "views/inventory_configuration.xml",
         "views/product_template.xml",
-=======
         "views/res_configuration.xml",
->>>>>>> 2727db268a2f47b31d7d6bb4f7f0ee5cadce0702
         "views/sale_order_view.xml",
         "wizard/sale_order_confirm_wiz_view.xml",
     ],
