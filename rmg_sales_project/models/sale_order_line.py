85c2bae69177a9f000ee8eb77080c111d0c6fe96
KhyatiAktiv <khyati@aktivsoftware.com>
2022-09-05 10:48:48 +0530

    [MOD]rmg_sales_project add user error on delete order line.

---
+++ rmg_sales_project/models/sale_order_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def unlink(self):
+        if self.project_id:
+            raise UserError(_("You cannot delete a order line containing project. "
+                              "You can either archive it or first delete its project."))
+        return super(SaleOrderLine, self).unlink()
