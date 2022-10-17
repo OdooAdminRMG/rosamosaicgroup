15412523bcadd36a4364d34fdd706c8e379f3b33
KhyatiAktiv <khyati@aktivsoftware.com>
2022-10-17 14:26:12 +0530

    [WIP]stock_unreserve_base pass context

---
+++ stock_unreserve_base/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock Unreserve Base",
+    "description":
+        """
+            Tasks: 'Update MO costing report source data', 
+            User Stories:
+        """,
+    "category": "stock",
+    "summary": "",
+    "sequence": 20,
+    "version": "15.0.0.0.0",
+    'author': "S4 Solutions, LLC",
+    'website': "https://www.sfour.io/",
+    "depends": ["stock"],
+    "data": [
+        "views/picking_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
