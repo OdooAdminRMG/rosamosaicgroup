bfad9a3bb2fd87da22ee0aff82dd7361421fb740
Odoo Aktiv <odoo@aktivsoftware.com>
2022-11-08 15:58:34 +0530

    [IMP] Completed changes of sale order line product replacement.

--- rmg_sales_selection_sheet/models/__init__.py
+++ rmg_sales_selection_sheet/models/__init__.py
@@ -1,4 +1,4 @@
 from . import (  # product_category,; hr_department,; res_company,
     corner_treatments, customer_sink_types, edge_profiles, faucets,
     mrp_production, range_types, rmg_sale, sale_order,
-    selection_sheet_configuration, stock_move,add_product)
+    selection_sheet_configuration, stock_move,add_product, stock_picking)
