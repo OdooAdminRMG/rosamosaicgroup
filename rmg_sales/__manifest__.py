719ac3f18b79d47c88af9f9d55ab24fc9193baf7
OdooAktiv <OdooAktiv@gmail.com>
2023-03-21 15:53:53 +0530

    [MOD]rmg_sales add confirm attribute and dialog

--- rmg_sales/__manifest__.py
+++ rmg_sales/__manifest__.py
@@ -12,7 +12,7 @@
     "author": "S4 Solutions, LLC",
     "website": "https://www.sfour.io/",
     "category": "business",
-    "version": "15.0.2.0.3",
+    "version": "15.0.2.1.0",
     "depends": ["sale_management", "sale_stock", 'stock'],
     "data": [
         "security/ir.model.access.csv",
@@ -23,6 +23,15 @@
         "views/sale_order_view.xml",
         "wizard/sale_order_confirm_wiz_view.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+        ],
+        "web.assets_backend": [
+            "rmg_sales/static/src/xml/dialog.xml",
+        ],
+        "web.assets_common": [
+        ]
+    },
     "installable": True,
     "application": True,
     "auto_install": False,
